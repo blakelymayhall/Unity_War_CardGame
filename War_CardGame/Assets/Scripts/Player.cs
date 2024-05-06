929d5a490dcc15d2aabe39250bef90bc8fe667e0
Blakely Mayhall <blakely.c.mayhall@gmail.com>
2024-05-05 20:35:46 -0500

    code cleanup

--- War_CardGame/Assets/Scripts/Player.cs
+++ War_CardGame/Assets/Scripts/Player.cs
@@ -8,4 +8,19 @@ public class Player : MonoBehaviour
     public HandOutcomes handOutcome = HandOutcomes.NoCardsPlayed;
     public string playerName = "Human";
 
+    public bool PlayerWin()
+    {
+        return handOutcome == HandOutcomes.Win;
+    }
+
+    public bool PlayerLose()
+    {
+        return handOutcome == HandOutcomes.Lose;
+    }
+
+    public bool PlayerDraw()
+    {
+        return handOutcome == HandOutcomes.Draw;
+    }
+
 }
